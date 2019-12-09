6adb728675d8b5ada2b6787f0a65c3bf17e193ee
adrianb199 <58709511+adrianb199@users.noreply.github.com>
2019-12-09 23:18:21 +0100

    ado

---
+++ Assets/Inventory_script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_Script : MonoBehaviour
+{
+	[SerializeField] private GameObject Inventory;
+
+	[SerializeField] private GameObject hud;
+
+	[SerializeField] private bool isPaused;
+
+	[SerializeField] private GameObject player;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			isPaused = !isPaused;
+		}
+
+		if (isPaused)
+		{
+			ActivateMenu();
+		}
+
+		else
+		{
+			DeactivateMenu();
+		}
+	}
+
+	void ActivateMenu()
+	{
+		Time.timeScale = 0;
+		AudioListener.pause = true;
+		Inventory.SetActive(true);
+		hud.SetActive(false);
+		player.SetActive(false);
+	}
+
+	public void DeactivateMenu()
+	{
+		Time.timeScale = 1;
+		AudioListener.pause = false;
+		Inventory.SetActive(false);
+		isPaused = false;
+		hud.SetActive(true);
+		player.SetActive(true);
+	}
+}
